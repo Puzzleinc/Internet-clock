2da5fd9aa315f9cd90ad9d3293558362eb2cd712
Puzzleinc <puzzleinc.design@gmail.com>
2023-03-12 22:33:37 +0700

    Fix NTP code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #include "wificonnect.h"
 
@@ -10,6 +12,22 @@ const char* deviceName;
 
 // Initialize Internal led build in
 uint8_t connled = 2;
+
+// Iniotialize NTP Class
+WiFiUDP ntpUDP;
+
+// By default 'pool.ntp.org' is used with 60 seconds update interval and
+// no offset
+// NTPClient timeClient(ntpUDP);
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionally you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+// NTPClient timeClient(ntpUDP, "pool.ntp.org", 25200, 60000); // Menambahkan variabel langsung pada fungsi
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 void setup() {
  /*  Common cathode led RGB */
@@ -25,10 +43,31 @@ void setup() {
   wificonnect(ssid, password, deviceName, connled);
 
   Serial.println(WiFi.gatewayIP());
+
+  timeClient.begin();
+  // Tambahan fungsi
+  timeClient.setTimeOffset(25200);
+  timeClient.setUpdateInterval(180000);
+
 }
 
 void loop() {
   // Millis Function #1
   unsigned long currentTime = millis() / 60000;
-  //Serial.println(currentTime);
+
+  timeClient.update();
+
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getEpochTime());
+  Serial.println(",");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  //Serial.println(timeClient.getFormattedTime());
+
+  delay(1000);
+  Serial.println(currentTime);
 }
