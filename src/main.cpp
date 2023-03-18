7608dbfe62a98d0dbd373ebb83d5141cc9f0f3d0
Puzzleinc <puzzleinc.design@gmail.com>
2023-03-19 01:38:14 +0700

    Add internal clock code

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,22 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org");
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 
+// Clock variable
+unsigned long timeLast = 0;
+unsigned long previousMillis = 0;
+unsigned long intervalDays = 2*24*60*60*1000; // 2 Days interval
+unsigned long intervalSec = 2; // 2 Days interval
+
+// set your starting hour here, not below at int hour. This ensures accurate daily correction of time
+
+uint8_t seconds;
+uint8_t minutes;
+uint8_t hours;
+uint8_t days;
+
+void updateTime(uint8_t seconds, uint8_t minutes, uint8_t hours, uint8_t days);
+void showTime(uint8_t seconds, uint8_t minutes, uint8_t hours, uint8_t days);
+
 void setup() {
  /*  Common cathode led RGB */
   Serial.begin(115200);
@@ -49,25 +65,95 @@ void setup() {
   timeClient.setTimeOffset(25200);
   timeClient.setUpdateInterval(180000);
 
+  timeClient.update();
+  seconds = timeClient.getSeconds();
+  minutes = timeClient.getMinutes();
+  hours = timeClient.getHours();
 }
 
 void loop() {
   // Millis Function #1
-  unsigned long currentTime = millis() / 60000;
+  unsigned long currentMillis = millis() / 1000;  // the number of milliseconds that have passed since boot
+  seconds = currentMillis - previousMillis;
 
-  timeClient.update();
+  //the number of seconds that have passed since the last time 60 seconds was reached.
+  if (seconds == 60) {
+    previousMillis = currentMillis;
+    seconds = 0;
+    minutes = minutes + 1; 
+  }
 
-  Serial.print(daysOfTheWeek[timeClient.getDay()]);
-  Serial.print(", ");
-  Serial.print(timeClient.getEpochTime());
-  Serial.println(",");
-  Serial.print(timeClient.getHours());
-  Serial.print(":");
-  Serial.print(timeClient.getMinutes());
-  Serial.print(":");
-  Serial.println(timeClient.getSeconds());
+  //if one minute has passed, start counting milliseconds from zero again and add one minute to the clock.
+  if (minutes == 60){
+    minutes = 0;
+    hours = hours + 1; 
+  }
+
+  if (hours == 24){
+    hours = 0;
+    days = days + 1; 
+  }
+
+  // if one hour has passed, start counting minutes from zero and add one hour to the clock
+  if (days == intervalDays){
+    timeClient.update();
+    hours = timeClient.getHours();
+    minutes = timeClient.getMinutes();
+    seconds = timeClient.getSeconds();
+  }
+
+  if (currentMillis - timeLast >= intervalDays) {
+    // save the last time you ran the function
+    timeLast = currentMillis;
+
+    // call the function
+    updateTime(seconds, minutes, hours, days);
+  }
+
+  if (currentMillis - timeLast >= intervalSec) {
+    // save the last time you ran the function
+    timeLast = currentMillis;
+
+    // call the function
+    showTime(seconds, minutes, hours, days);
+  }
+
+  // timeClient.update();
+
+  // Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  // Serial.print(", ");
+  // Serial.print(timeClient.getEpochTime());
+  // Serial.println(",");
+  // Serial.print(timeClient.getHours());
+  // Serial.print(":");
+  // Serial.print(timeClient.getMinutes());
+  // Serial.print(":");
+  // Serial.println(timeClient.getSeconds());
   //Serial.println(timeClient.getFormattedTime());
 
-  delay(1000);
-  Serial.println(currentTime);
+  // delay(1000);
+  // Serial.println(currentTime);
+}
+
+void updateTime(uint8_t seconds, uint8_t minutes, uint8_t hours, uint8_t days) {
+  // your code here
+  timeClient.update();
+  seconds = timeClient.getSeconds();
+  minutes = timeClient.getMinutes();
+  hours = timeClient.getHours();
+}
+
+void showTime(uint8_t seconds, uint8_t minutes, uint8_t hours, uint8_t days) {
+  // your code here
+  timeClient.update();
+  Serial.print("Waktu internal: ");
+  Serial.print(days);
+  Serial.print(":");
+  Serial.print(hours);
+  Serial.print(":");
+  Serial.print(minutes);
+  Serial.print(":");
+  Serial.println(seconds);
+  Serial.print(" -- ");
+  Serial.println("Waktu internet : " + timeClient.getFormattedTime());
 }
